673d490eb78a8cdfb4291ce1206928605123def0
yutaka <jianghewaseda0503@yahoo.co.jp>
2019-01-25 15:50:44 +0900

    change csv output format

--- src/offline/tool/brake.py
+++ src/offline/tool/brake.py
@@ -8,6 +8,7 @@ import plotly
 from plotly.graph_objs import Surface
 from plotly.graph_objs import Scatter3d
 
+
 class Brake:
 
     def __init__(self, data, resultDirectory, speedGroupNum=60, brakeGroupNum=25, speedRange=80, brakeRange=1.0,
@@ -26,18 +27,22 @@ class Brake:
         '''
         __speedRef = np.arange(0, self.speedRange, self.speedInterval)
         __brakeRef = np.arange(0, self.brakeRange, self.brakeInterval)
-        self.speedResult, self.brakeResult = np.meshgrid(__speedRef, __brakeRef)
+
+        # meshgriを作成
+        self.brakeResult, self.speedResult = np.meshgrid(__brakeRef, __speedRef)
+
         self.speedRefNum = int(self.speedRange / self.speedInterval)
         self.brakeRefNum = int(self.brakeRange / self.brakeInterval)
 
         self.__brakeModelGrouping(speedGroupNum, brakeGroupNum)
         self.__findOutliers()
+        self.__createTrainingData()
 
         if train:
             self.__train(save)
         else:
-            self.model = model_from_json(open(self.resultDirectory+'TrainedModel/brakeModel.json').read())
-            self.model.load_weights(self.resultDirectory+'TrainedModel/brakeModel.h5')
+            self.model = model_from_json(open(self.resultDirectory + 'TrainedModel/brakeModel.json').read())
+            self.model.load_weights(self.resultDirectory + 'TrainedModel/brakeModel.h5')
 
         self.__refer()
 
@@ -61,10 +66,10 @@ class Brake:
         self.dfPacmod["speedKey"] = pd.cut(self.dfPacmod.speed, speedGroupNum,
                                            labels=[i + 1 for i in range(speedGroupNum)])
         self.dfPacmod["brakeKey"] = pd.cut(self.dfPacmod.brake, brakeGroupNum,
-                                              labels=[i + 1 for i in range(brakeGroupNum)])
+                                           labels=[i + 1 for i in range(brakeGroupNum)])
         _groupListAcc = list(self.dfPacmod.groupby(["speedKey", "brakeKey"], as_index=False).groups.keys())
         self.dfPacmod["groupBrake"] = self.dfPacmod.apply(lambda row: _groupListAcc.index((row["speedKey"],
-                                                                                         row["brakeKey"])), axis=1)
+                                                                                           row["brakeKey"])), axis=1)
 
         self.groupBrakeMeanList = self.dfPacmod.groupby("groupBrake").mean()["accFiltered"].values
         self.groupBrakeStdList = self.dfPacmod.groupby("groupBrake").std(ddof=0)["accFiltered"].values
@@ -81,61 +86,63 @@ class Brake:
         model.compile(optimizer="Adam", loss="mean_squared_error")
         self.model = model
 
+    def __createTrainingData(self):
+        self.dfPacmodTrain = self.dfPacmod.query("remove==0 & speed>8").copy()
+        self._accMinNum = self.dfPacmodTrain["accFiltered"].min()
+        self._accMaxNum = self.dfPacmodTrain["accFiltered"].max()
+
     def __train(self, save):
-        dfPacmodTrain = self.dfPacmod.query("remove==0 & speed>8").copy()
-        self._accMinNum = dfPacmodTrain["accFiltered"].min()
-        self._accMaxNum = dfPacmodTrain["accFiltered"].max()
-        x_train = np.array([dfPacmodTrain["speed"] / dfPacmodTrain["speed"].max(), dfPacmodTrain["brake"]]).T
-        y_train = (dfPacmodTrain["accFiltered"].values - self._accMinNum) / (self._accMaxNum - self._accMinNum)
+        x_train = np.array([self.dfPacmodTrain["brake"], self.dfPacmodTrain["speed"] / self.dfPacmodTrain["speed"].max()]).T
+        y_train = (self.dfPacmodTrain["accFiltered"].values - self._accMinNum) / (self._accMaxNum - self._accMinNum)
 
         self.__createNetwork()
         self.model.fit(x_train, y_train, batch_size=128, epochs=300, verbose=1, validation_split=0.2, shuffle=True)
 
         if save:
-            json_string=self.model.to_json()
-            open(self.resultDirectory+'TrainedModel/brakeModel.json', 'w').write(json_string)
-            self.model.save_weights(self.resultDirectory+'TrainedModel/brakeModel.h5')
+            json_string = self.model.to_json()
+            open(self.resultDirectory + 'TrainedModel/brakeModel.json', 'w').write(json_string)
+            self.model.save_weights(self.resultDirectory + 'TrainedModel/brakeModel.h5')
 
     def __refer(self):
         _predict = []
-        for i in range(0, self.brakeRefNum):
-            _predict.append(self.model.predict(np.array([self.speedResult[i] / self.speedResult[i].max(),
-                                                         self.brakeResult[i]]).T))
+        for i in range(0, self.speedRefNum):
+            _predict.append(self.model.predict(np.array([self.brakeResult[i],
+                                                         self.speedResult[i] / self.speedResult[:, 0].max()]).T))
 
-        self.predictedAcceleration = np.array(_predict).reshape(self.brakeRefNum, self.speedRefNum)
+        self.predictedAcceleration = np.array(_predict).reshape(self.speedRefNum, self.brakeRefNum)
 
     def getGraphResult(self):
-        _representSpeed= self.dfPacmod.query("remove==0").groupby("groupBrake").mean()["speed"].values
-        _representBrake= self.dfPacmod.query("remove==0").groupby("groupBrake").mean()["brake"].values
-        _representAcceleration= self.dfPacmod.query("remove==0").groupby("groupBrake").mean()["accFiltered"].values
+        _representSpeed = self.dfPacmod.query("remove==0").groupby("groupBrake").mean()["speed"].values
+        _representBrake = self.dfPacmod.query("remove==0").groupby("groupBrake").mean()["brake"].values
+        _representAcceleration = self.dfPacmod.query("remove==0").groupby("groupBrake").mean()["accFiltered"].values
 
-        _surface = Surface(x=self.speedResult, y=self.brakeResult,
-                          z=self.predictedAcceleration * (self._accMaxNum - self._accMinNum) + self._accMinNum,
-                          colorscale='YlGnBu', name="predictSurface")
+        _surface = Surface(x=self.brakeResult, y=self.speedResult,
+                           z=self.predictedAcceleration * (self._accMaxNum - self._accMinNum) + self._accMinNum,
+                           colorscale='YlGnBu', name="predictSurface")
 
-        _scatterRepresent = Scatter3d(x=_representSpeed, y=_representBrake, z=_representAcceleration, mode='markers',
-                                marker=dict(size=2, color="green"), name="Boss")
+        _scatterRepresent = Scatter3d(x=_representBrake, y=_representSpeed, z=_representAcceleration, mode='markers',
+                                      marker=dict(size=2, color="green"), name="Boss")
 
-        _scatterRemove = Scatter3d(x=self.dfPacmod.query("remove==0").speed, y=self.dfPacmod.query("remove==0").brake,
-                                  z=self.dfPacmod.query("remove==0").accFiltered, mode='markers',
-                                  marker=dict(size=3, color="blue"), name="RemoveOutliers")
+        _scatterRemove = Scatter3d(x=self.dfPacmod.query("remove==0").brake, y=self.dfPacmod.query("remove==0").speed,
+                                   z=self.dfPacmod.query("remove==0").accFiltered, mode='markers',
+                                   marker=dict(size=3, color="blue"), name="RemoveOutliers")
 
-        layout = go.Layout(title="ThrottleModel", scene=dict(xaxis=dict(title="speed", range=[80, 0]),
-                                                             yaxis=dict(title="CMD(Brake)", range=[0.1, 0.7]),
+        layout = go.Layout(title="ThrottleModel", scene=dict(xaxis=dict(title="CMD(Brake)", range=[0, 0.7]),
+                                                             yaxis=dict(title="speed", range=[0, 80]),
                                                              zaxis=dict(title="acceleration", range=[-10, 0])))
 
         fig = go.Figure(data=[_surface, _scatterRepresent, _scatterRemove], layout=layout)
 
         _figName = "brake.html"
-        print("saveGraphFileName:" + self.resultDirectory+_figName)
-        plotly.offline.plot(fig, filename=self.resultDirectory+_figName)
+        print("saveGraphFileName:" + self.resultDirectory + _figName)
+        plotly.offline.plot(fig, filename=self.resultDirectory + _figName)
 
     def getCsvResult(self):
         _accResult = self.predictedAcceleration * (self._accMaxNum - self._accMinNum) + self._accMinNum
-        output = pd.DataFrame({'speed':self.speedResult.reshape(-1, ),
-                               'brake':self.brakeResult.reshape(-1, ),
+        output = pd.DataFrame({'brake': self.brakeResult.reshape(-1, ),
+                               'speed': self.speedResult.reshape(-1, ),
                                'acceleration': _accResult.reshape(-1, )})
 
-        output = output.round({'brake':2, 'acceleration':2})
+        output = output.round({'brake': 2, 'acceleration': 2})
 
-        output.to_csv(self.resultDirectory+"brake.csv", index=False)
+        output.to_csv(self.resultDirectory + "brake.csv", index=False)
--- src/offline/tool/throttle.py
+++ src/offline/tool/throttle.py
@@ -27,18 +27,22 @@ class Throttle:
         '''
         __speedRef = np.arange(0, self.speedRange, self.speedInterval)
         __throttleRef = np.arange(0, self.throttleRange, self.throttleInterval)
-        self.speedResult, self.throttleResult = np.meshgrid(__speedRef, __throttleRef)
+
+        # meshgridを作成
+        self.throttleResult, self.speedResult = np.meshgrid(__throttleRef, __speedRef)
+
         self.speedRefNum = int(self.speedRange / self.speedInterval)
         self.throttleRefNum = int(self.throttleRange / self.throttleInterval)
 
         self.__throttleModelGrouping(speedGroupNum, throttleGroupNum)
         self.__findOutliers()
+        self.__createTrainingData()
 
         if train:
             self.__train(save)
         else:
-            self.model = model_from_json(open(self.resultDirectory+'TrainedModel/throttleModel.json').read())
-            self.model.load_weights(self.resultDirectory+'TrainedModel/throttleModel.h5')
+            self.model = model_from_json(open(self.resultDirectory + 'TrainedModel/throttleModel.json').read())
+            self.model.load_weights(self.resultDirectory + 'TrainedModel/throttleModel.h5')
 
         self.__refer()
 
@@ -65,7 +69,8 @@ class Throttle:
                                               labels=[i + 1 for i in range(throttleGroupNum)])
         _groupListAcc = list(self.dfPacmod.groupby(["speedKey", "throttleKey"], as_index=False).groups.keys())
         self.dfPacmod["groupThrottle"] = self.dfPacmod.apply(lambda row: _groupListAcc.index((row["speedKey"],
-                                                                                         row["throttleKey"])), axis=1)
+                                                                                              row["throttleKey"])),
+                                                             axis=1)
 
         self.groupThrottleMeanList = self.dfPacmod.groupby("groupThrottle").mean()["accFiltered"].values
         self.groupThrottleStdList = self.dfPacmod.groupby("groupThrottle").std(ddof=0)["accFiltered"].values
@@ -82,61 +87,66 @@ class Throttle:
         model.compile(optimizer="Adam", loss="mean_squared_error")
         self.model = model
 
+    def __createTrainingData(self):
+        self.dfPacmodTrain = self.dfPacmod.query("remove==0 & speed<80").copy()
+        self._accMinNum = self.dfPacmodTrain["accFiltered"].min()
+        self._accMaxNum = self.dfPacmodTrain["accFiltered"].max()
+
+
     def __train(self, save):
-        dfPacmodTrain = self.dfPacmod.query("remove==0 & speed<80").copy()
-        self._accMinNum = dfPacmodTrain["accFiltered"].min()
-        self._accMaxNum = dfPacmodTrain["accFiltered"].max()
-        x_train = np.array([dfPacmodTrain["speed"] / dfPacmodTrain["speed"].max(), dfPacmodTrain["accel"]]).T
-        y_train = (dfPacmodTrain["accFiltered"].values - self._accMinNum) / (self._accMaxNum - self._accMinNum)
+
+        # dataset
+        x_train = np.array([self.dfPacmodTrain["accel"], self.dfPacmodTrain["speed"] / self.dfPacmodTrain["speed"].max()]).T
+        y_train = (self.dfPacmodTrain["accFiltered"].values - self._accMinNum) / (self._accMaxNum - self._accMinNum)
 
         self.__createNetwork()
         self.model.fit(x_train, y_train, batch_size=128, epochs=300, verbose=1, validation_split=0.2, shuffle=True)
 
         if save:
-            json_string=self.model.to_json()
-            open(self.resultDirectory+'TrainedModel/throttleModel.json', 'w').write(json_string)
-            self.model.save_weights(self.resultDirectory+'TrainedModel/throttleModel.h5')
+            json_string = self.model.to_json()
+            open(self.resultDirectory + 'TrainedModel/throttleModel.json', 'w').write(json_string)
+            self.model.save_weights(self.resultDirectory + 'TrainedModel/throttleModel.h5')
 
     def __refer(self):
         _predict = []
-        for i in range(0, self.throttleRefNum):
-            _predict.append(self.model.predict(np.array([self.speedResult[i] / self.speedResult[i].max(),
-                                                         self.throttleResult[i]]).T))
+        for i in range(0, self.speedRefNum):
+            _predict.append(self.model.predict(np.array([self.throttleResult[i],
+                                                        self.speedResult[i] / self.speedResult[:, 0].max()]).T))
 
-        self.predictedAcceleration = np.array(_predict).reshape(self.throttleRefNum, self.speedRefNum)
+        self.predictedAcceleration = np.array(_predict).reshape(self.speedRefNum, self.throttleRefNum)
 
     def getGraphResult(self):
-        _representSpeed= self.dfPacmod.query("remove==0").groupby("groupThrottle").mean()["speed"].values
-        _representThrottle= self.dfPacmod.query("remove==0").groupby("groupThrottle").mean()["accel"].values
-        _representAcceleration= self.dfPacmod.query("remove==0").groupby("groupThrottle").mean()["accFiltered"].values
+        _representSpeed = self.dfPacmod.query("remove==0").groupby("groupThrottle").mean()["speed"].values
+        _representThrottle = self.dfPacmod.query("remove==0").groupby("groupThrottle").mean()["accel"].values
+        _representAcceleration = self.dfPacmod.query("remove==0").groupby("groupThrottle").mean()["accFiltered"].values
 
-        _surface = Surface(x=self.speedResult, y=self.throttleResult,
-                          z=self.predictedAcceleration * (self._accMaxNum - self._accMinNum) + self._accMinNum,
-                          colorscale='YlGnBu', name="predictSurface")
+        _surface = Surface(x=self.throttleResult, y=self.speedResult,
+                           z=self.predictedAcceleration * (self._accMaxNum - self._accMinNum) + self._accMinNum,
+                           colorscale='YlGnBu', name="predictSurface")
 
-        _scatterRepresent = Scatter3d(x=_representSpeed, y=_representThrottle, z=_representAcceleration, mode='markers',
-                                marker=dict(size=2, color="green"), name="Boss")
+        _scatterRepresent = Scatter3d(x=_representThrottle, y=_representSpeed, z=_representAcceleration, mode='markers',
+                                      marker=dict(size=2, color="green"), name="Boss")
 
-        _scatterRemove = Scatter3d(x=self.dfPacmod.query("remove==0").speed, y=self.dfPacmod.query("remove==0").accel,
-                                  z=self.dfPacmod.query("remove==0").accFiltered, mode='markers',
-                                  marker=dict(size=3, color="blue"), name="RemoveOutliers")
+        _scatterRemove = Scatter3d(x=self.dfPacmod.query("remove==0").accel, y=self.dfPacmod.query("remove==0").speed,
+                                   z=self.dfPacmod.query("remove==0").accFiltered, mode='markers',
+                                   marker=dict(size=3, color="blue"), name="RemoveOutliers")
 
-        layout = go.Layout(title="ThrottleModel", scene=dict(xaxis=dict(title="speed", range=[0, 80]),
-                                                             yaxis=dict(title="CMD(Throttle)", range=[0.1, 0.7]),
+        layout = go.Layout(title="ThrottleModel", scene=dict(xaxis=dict(title="CMD(Throttle)", range=[0.1, 0.7]),
+                                                             yaxis=dict(title="speed", range=[80, 0]),
                                                              zaxis=dict(title="acceleration", range=[0, 10])))
 
         fig = go.Figure(data=[_surface, _scatterRepresent, _scatterRemove], layout=layout)
 
         _figName = "throttle.html"
-        print("saveGraphFileName:" + self.resultDirectory+_figName)
-        plotly.offline.plot(fig, filename=self.resultDirectory+_figName)
+        print("saveGraphFileName:" + self.resultDirectory + _figName)
+        plotly.offline.plot(fig, filename=self.resultDirectory + _figName)
 
     def getCsvResult(self):
         _accResult = self.predictedAcceleration * (self._accMaxNum - self._accMinNum) + self._accMinNum
-        output = pd.DataFrame({'speed':self.speedResult.reshape(-1, ),
-                               'throttle':self.throttleResult.reshape(-1, ),
+        output = pd.DataFrame({'throttle': self.throttleResult.reshape(-1, ),
+                               'speed': self.speedResult.reshape(-1, ),
                                'acceleration': _accResult.reshape(-1, )})
 
-        output = output.round({'throttle':2, 'acceleration':2})
+        output = output.round({'throttle': 2, 'acceleration': 2})
 
-        output.to_csv(self.resultDirectory+"throttle.csv", index=False)
+        output.to_csv(self.resultDirectory + "throttle.csv", index=False)
